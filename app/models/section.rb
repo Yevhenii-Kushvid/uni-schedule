04de4d125c68b5fd2ca95055cb3145aa602c7795
Yevhenii Kushvid <pathfinder1994@mail.ru>
2024-12-16 20:52:23 +0200

    add weekly periodity humanization, outof bitemask

--- app/models/section.rb
+++ app/models/section.rb
@@ -12,4 +12,22 @@ class Section < ApplicationRecord
     self.subject_name   = subject.name if teacher_id_changed?
     self.classroom_name = classroom.name if teacher_id_changed?
   end
+
+  def weekly_periodity_humanize
+    weekly_bitemask.filter { |day, mask| !(weekly_periodity & mask).zero? }.keys
+  end
+
+  private
+
+  def weekly_bitemask
+    {
+      sunday:       0b0000001,
+      monday:       0b0000010,
+      tuesday:      0b0000100,
+      wednesday:    0b0001000,
+      thrusday:     0b0010000,
+      friday:       0b0100000,
+      saturday:     0b1000000
+    }
+  end
 end
